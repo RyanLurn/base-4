3ab5fce630ad7e294c980d36e49c46cd691235f2
Ryan Luong <lumintriet@gmail.com>
2026-09-23 09:05:22 +0700

    build(config): define vite config.

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    // Make sure that '@tanstack/react-start/plugin/vite' is passed before '@vitejs/plugin-react'
+    tanstackStart({
+      router: {
+        quoteStyle: "double",
+        semicolons: true,
+      },
+    }),
+    react(),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  server: {
+    port: 5173,
+  },
+});
