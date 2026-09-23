e10da4d76b115483c4791a0dc6788ddceaa0770b
Ryan Luong <lumintriet@gmail.com>
2026-09-23 09:06:47 +0700

    feat(router): define getRouter function.

---
+++ src/router.ts
@@ -0,0 +1,12 @@
+import { createRouter } from "@tanstack/react-router";
+
+import { routeTree } from "@/routeTree.gen";
+
+export function getRouter() {
+  const router = createRouter({
+    scrollRestoration: true,
+    routeTree,
+  });
+
+  return router;
+}
